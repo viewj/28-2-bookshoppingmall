0301fabd4759ee691e4e570e192a86546d06f17b
Administrator <Administrator@smart20115>
2018-07-18 10:54:51 +0900

    [서연문] 2018-07-18
    1. DB 연결을 위한 클래스 및 메서드 생성
    	( DButil.java 추가 )
    
    	- DButil 클래스 내부에 connectDB 메서드를 선언하였습니다.

---
+++ 28-2 bookshoppingmall/src/service/DButil.java
@@ -0,0 +1,25 @@
+package service;
+import java.sql.*;
+
+public class DButil {
+
+	public static Connection connectDB() {
+		Connection conn = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			String jdbcDriver = "jdbc:mysql://localhost:3306/bookshop?useUnicode=true&characterEncoding=euckr";
+			String dbUser = "root";
+			String dbPw = "java0000";
+			
+			conn = DriverManager.getConnection(jdbcDriver, dbUser, dbPw);
+			 
+		} catch (ClassNotFoundException e) {
+			System.out.println("DB Driver Class를 찾지 못하였습니다. 커넥터를 확인해주세요!, connectDB");
+		} catch (SQLException e) {
+			System.out.println("DB와 관련된 예외가 발생하였습니다, connectDB");
+			e.printStackTrace();
+		}
+		return conn;			 
+	}
+}
