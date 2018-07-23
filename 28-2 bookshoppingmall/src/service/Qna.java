a18be42ef2ef7087b2493b73acdab0b4aa93a6e9
정규룡 <jgl25@naver.com>
2018-07-23 16:01:48 +0900

    [정규룡, 전재현] - 2018-07-23
    1. Qna.java dto 작성완료
    	Qna테이블의 속성 각각의 데이터 타입 지정 및 get / set 메소드
    2. QnaDao.java dao
    	insert, update, delete, select(한 행 검색), selectAll(전체 행 검색) 메소드 작성
    3. QnaService.java service
    	insert, update, delete, select(한 행 검색), selectAll(전체 행 검색) 서비스 작성

---
+++ 28-2 bookshoppingmall/src/service/Qna.java
@@ -0,0 +1,51 @@
+// 2018.07.23 정규룡, 전재현 짝코딩
+package service;
+
+public class Qna {
+	private int qna_no;
+	private int member_no;
+	private String qna_title;
+	private String qna_content;
+	private String qna_date;
+	
+	public void setQna_no(int qna_no) {
+		this.qna_no = qna_no; 
+	}
+	
+	public int getQna_no() {
+		return qna_no;
+	}
+
+	public int getMember_no() {
+		return member_no;
+	}
+
+	public void setMember_no(int member_no) {
+		this.member_no = member_no;
+	}
+
+	public String getQna_title() {
+		return qna_title;
+	}
+
+	public void setQna_title(String qna_title) {
+		this.qna_title = qna_title;
+	}
+
+	public String getQna_content() {
+		return qna_content;
+	}
+
+	public void setQna_content(String qna_content) {
+		this.qna_content = qna_content;
+	}
+
+	public String getQna_date() {
+		return qna_date;
+	}
+
+	public void setQna_date(String qna_date) {
+		this.qna_date = qna_date;
+	}
+	
+}
