491745fa806c8d711b3c274fa605d3f7bfb92e53
김정연 <wjddus1277@naver.com>
2018-07-23 11:31:18 +0900

    [김정연] 2018-07-23
    1. 클래스 생성.
    
    	-QnaComment.java
    	-QnaCommentDao.java
    
    2. qna 댓글 수정처리.
    
    	-QnaCommentDao에 select, update를 이용한 수정처리 완료.
    
    3. QnaComment.java set, get.

---
+++ 28-2 bookshoppingmall/src/service/QnaComment.java
@@ -0,0 +1,39 @@
+package service;
+
+public class QnaComment {
+	private int qnaCommentNo;
+	private int qnaNo;
+	private int adminNo;
+	private String qnaCommentContent;
+	private String qnaCommentDate;
+	public int getQnaCommentNo() {
+		return qnaCommentNo;
+	}
+	public void setQnaCommentNo(int qnaCommentNo) {
+		this.qnaCommentNo = qnaCommentNo;
+	}
+	public int getQnaNo() {
+		return qnaNo;
+	}
+	public void setQnaNo(int qnaNo) {
+		this.qnaNo = qnaNo;
+	}
+	public int getAdminNo() {
+		return adminNo;
+	}
+	public void setAdminNo(int adminNo) {
+		this.adminNo = adminNo;
+	}
+	public String getQnaCommentContent() {
+		return qnaCommentContent;
+	}
+	public void setQnaCommentContent(String qnaCommentContent) {
+		this.qnaCommentContent = qnaCommentContent;
+	}
+	public String getQnaCommentDate() {
+		return qnaCommentDate;
+	}
+	public void setQnaCommentDate(String qnaCommentDate) {
+		this.qnaCommentDate = qnaCommentDate;
+	}
+}
---
+++ 28-2 bookshoppingmall/src/service/QnaCommentDao.java
@@ -0,0 +1,87 @@
+package service;
+
+import java.sql.*;
+import service.*;
+
+public class QnaCommentDao {
+	//QnaComment 클래스를 리턴 데이터 타입 지정. selectForUpdateQnaComment 메서드 선언. 매개변수로는 db연결을 위한 conn과 int 데이터 타입인 adminNo(session)과 qnaNo(외래키)을 선언.
+	public QnaComment selectForUpdateQnaComment(Connection conn, int adminNo, int qnaNo) {
+		//초기값 지정
+		PreparedStatement pstmtSelectForUpdateQnaComment = null;
+		ResultSet rsSelectForUpdateQnaComment = null;
+		//QnaComment 클래스에 대한 객체 생성.
+		QnaComment qnaComment = new QnaComment();
+		//쿼리문 변수에 대입.
+		String sqlSelectForUpdateQnaComment = "SELECT qna_comment_no, qna_no, admin_no, qna_comment_content FROM qna_comment where qna_no=? and admin_no=?";
+		
+		try {
+			//PreparedStatement 객체 참조 변수에 db 실행을 위한 데이터 삽입.
+			pstmtSelectForUpdateQnaComment = conn.prepareStatement(sqlSelectForUpdateQnaComment);
+			//쿼리문의 ?에 값 입력.
+			pstmtSelectForUpdateQnaComment.setInt(1, qnaNo);
+			pstmtSelectForUpdateQnaComment.setInt(2, adminNo);
+			//쿼리 실행 정보를 ResultSet 객체참조 변수에 대입.
+			rsSelectForUpdateQnaComment = pstmtSelectForUpdateQnaComment.executeQuery();
+			//테이블 내에 생성된 데이터 값 추출.
+			if(rsSelectForUpdateQnaComment.next()) {
+				//추출된 데이터 값을 qnaComment 클래스 내부의 메서드에 셋팅.
+				qnaComment.setQnaCommentNo(rsSelectForUpdateQnaComment.getInt("qna_comment_no"));
+				qnaComment.setQnaNo(rsSelectForUpdateQnaComment.getInt("qna_no"));
+				qnaComment.setAdminNo(rsSelectForUpdateQnaComment.getInt("admin_no"));
+				qnaComment.setQnaCommentContent(rsSelectForUpdateQnaComment.getString("qna_comment_content"));
+			}
+		//예외처리
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(rsSelectForUpdateQnaComment != null) {
+				try {
+					rsSelectForUpdateQnaComment.close();
+				} catch(SQLException e) {
+					System.out.println("DB와 관련된 예외가 발생하였습니다, selectForUpdateQnaComment close");
+					e.printStackTrace();
+				}
+			}
+			if(pstmtSelectForUpdateQnaComment != null) {
+				try {
+					pstmtSelectForUpdateQnaComment.close();
+				} catch(SQLException e) {
+					System.out.println("DB와 관련된 예외가 발생하였습니다, selectForUpdateQnaComment close");
+					e.printStackTrace();
+				}
+			}
+		}
+		//셋팅된 데이터가 있는 qnaComment 클래스 리턴.
+		return qnaComment;
+		
+	}
+	//리턴 데이터 타입은 void. updateQnaComment 메서드 선언 후 매개변수로는 conn과 QnaComment 클래스 데이터 타입인 qnaComment를 선언.
+	public void updateQnaComment(Connection conn, QnaComment qnaComment) {
+		//초기값.
+		PreparedStatement pstmtUpdateQnaComment = null;
+		//String 데이터 타입에 쿼리문 대입.
+		String sqlUpdateQnaComment = "UPDATE qna_comment SET qna_comment_content=?, qna_comment_date=now() where qna_comment_no=?";
+		
+		try {
+			//db 작업을 위한 데이터 들을 PreparedStatement 객체참조변수에 대입.
+			pstmtUpdateQnaComment = conn.prepareStatement(sqlUpdateQnaComment);
+			//쿼리문의 ?값에 해당 값 삽입.
+			pstmtUpdateQnaComment.setString(1, qnaComment.getQnaCommentContent());
+			pstmtUpdateQnaComment.setInt(2, qnaComment.getQnaCommentNo());
+			//쿼리 실행.
+			pstmtUpdateQnaComment.executeUpdate();
+		//예외처리.
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if(pstmtUpdateQnaComment != null) {
+				try {
+					pstmtUpdateQnaComment.close();
+				} catch(SQLException e) {
+					System.out.println("DB와 관련된 예외가 발생하였습니다, updateQnaComment close");
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
